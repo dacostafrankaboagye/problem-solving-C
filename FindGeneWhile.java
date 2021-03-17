b4ba8b2b567d5c41eb6b25f90e0f13ff4d70166b
dacostafrankaboagye <60321537+dacostafrankaboagye@users.noreply.github.com>
2021-03-17 23:07:44 +0000

    Update FindGeneWhile.java

--- FindGeneWhile.java
+++ FindGeneWhile.java
@@ -1,9 +1,9 @@
 
 /**
- * Write a description of FindGeneWhile here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * 
+ * Name: Frank Aboagye
+ *
  */
 
 import edu.duke.*;
