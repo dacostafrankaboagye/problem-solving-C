ccc7c411361c147e06a55c2c69c85aca75bf6c4f
dacostafrankaboagye <60321537+dacostafrankaboagye@users.noreply.github.com>
2023-03-09 08:20:10 +0000

    Update tester_problem.java

--- tester_problem.java
+++ tester_problem.java
@@ -127,32 +127,108 @@ class Triangle {
 	}
 }
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.util.*;
+/*
+safe - 
 
-public class FindSafePolygon {
+*/
 
-	public static void main(String[] args) {
-		if (args.length < 1) {
-			System.out.println("Error: Must provide an input file");
-			return;
-		}
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-		// Read points from input file
-		List<Point> points = readPointsFromFile(args[0]);
 
-		// Find the largest convex polygon
-		ConvexPolygon polygon = findLargestConvexPolygon(points);
+public class FindSafeTriangle {
+    public static void main(String[] args) throws FileNotFoundException {
+        // read in file
+        File file = new File(args[0]);
+        Scanner scanner = new Scanner(file);
+        // read in points
+        List<Point> points = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            String[] line = scanner.nextLine().split(",");
+            double x = Double.parseDouble(line[0]);
+            double y = Double.parseDouble(line[1]);
+            points.add(new Point(x, y));
+        }
+        // find largest triangle
+        Triangle largestTriangle = findLargestTriangle(points);
+        // write results to file
+        largestTriangle.writeToFile();
+    }
 
-		// Write the vertices of the polygon to the output file
-		writePolygonToFile(polygon);
-	}
+    private static Triangle findLargestTriangle(List<Point> points) {
+        Triangle largestTriangle = new Triangle(null, null, null);
+        // sort points by x-coordinate
+        points.sort((p1, p2) -> Double.compare(p1.x, p2.x));
+        // loop through points
+        for (int i = 0; i < points.size(); i++) {
+            Point point1 = points.get(i);
+            // loop through points after point1
+            for (int j = i + 1; j < points.size(); j++) {
+                Point point2 = points.get(j);
+                // loop through points after point2
+                for (int k = j + 1; k < points.size(); k++) {
+                    Point point3 = points.get(k);
+                    // create triangle
+                    Triangle triangle = new Triangle(point1, point2, point3);
+                    // check if triangle is valid
+                    if (triangle.isValid()) {
+                        // check if triangle is larger than largest triangle
+                        if (triangle.area() > largestTriangle.area()) {
+                            largestTriangle = triangle;
+                        }
+                    }
+                }
+            }
+        }
+        return largestTriangle;
+    }
 
-	// Read points from the input file
-	private static List<Point> readPointsFromFile(String filename) {
-		List<Point> points = new ArrayList<>();
-		try {
-			FileReader fr = new FileReader(filename);
-			Scanner sc = new Scanner(fr);
-			
+    static class Point {
+        double x;
+        double y;
+        Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    static class Triangle {
+        Point p1;
+        Point p2;
+        Point p3;
+        Triangle(Point p1, Point p2, Point p3) {
+            this.p1 = p1;
+            this.p2 = p2;
+            this.p3 = p3;
+        }
+        // check if triangle is valid
+        boolean isValid() {
+            return (p1 != null && p2 != null && p3 != null);
+        }
+        // calculate area of triangle
+        double area() {
+            if (!isValid()) return 0;
+            return Math.abs(p1.x * (p2.y - p3.y) +
+                    p2.x * (p3.y - p1.y) +
+                    p3.x * (p1.y - p2.y)) / 2;
+        }
+        // write triangle to file
+        void writeToFile() throws FileNotFoundException {
+            if (!isValid()) return;
+            // create output file
+            File file = new File("safetriangle.csv");
+            // write to file
+            PrintWriter pw = new PrintWriter(file);
+            pw.println("X,Y");
+            pw.println(p1.x + "," + p1.y);
+            pw.println(p2.x + "," + p2.y);
+            pw.println(p3.x + "," + p3.y);
+            pw.close();
+        }
+    }
+}
+	
+	
